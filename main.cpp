10ba6c732a82f541f66d8af63eaa5ac8570385a8
NPlebanskii <nazar796@gmail.com>
2016-03-05 22:40:39 +0200

    Added pathToProject variable.

--- main.cpp
+++ main.cpp
@@ -8,32 +8,34 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QString projectPath = "D:\\NazarsProjectsProgramming\\Qt_projects\\TZI_1\\";
+
     HistogramWidget hwEnglish;
     hwEnglish.setColumnNumber(30);
     TextStatisticGenerator tsg;
-    tsg.setFileName("D:\\Qt_Projects\\TZI14\\TZI14\\englishText.txt");
+    tsg.setFileName(projectPath + "englishText.txt");
     hwEnglish.setData(tsg.generateSortedFreqStatistic(1));
     hwEnglish.show();
 
     HistogramWidget hwUkrainian;
-    tsg.setFileName("D:\\Qt_Projects\\TZI14\\TZI14\\ukrainianText.txt");
+    tsg.setFileName(projectPath + "ukrainianText.txt");
     hwUkrainian.setData(tsg.generateSortedAlphaStatistic(1));
     hwUkrainian.setColumnNumber(36);
     hwUkrainian.show();
 
     HistogramWidget hwTest;
-    tsg.setFileName("D:\\Qt_Projects\\TZI14\\TZI14\\englishText.txt");
+    tsg.setFileName(projectPath + "englishText.txt");
     hwTest.setColumnNumber(15);
     hwTest.setData(tsg.generateSortedFreqStatistic(2));
     hwTest.show();
 
     HistogramWidget hwTest2;
-    tsg.setFileName("D:\\Qt_Projects\\TZI14\\TZI14\\englishText.txt");
+    tsg.setFileName(projectPath + "englishText.txt");
     hwTest2.setColumnNumber(15);
-    hwTest2.setData(tsg.generateSortedAlphaStatistic(3));
+    hwTest2.setData(tsg.generateSortedFreqStatistic(3));
     hwTest2.show();
-    CezarDecoder cd("D:\\Qt_Projects\\TZI14\\TZI14\\SH_VT18");
 
+    CezarDecoder cd(projectPath + "SH_VT18");
     qDebug() << cd.decodedString();
 
     return a.exec();
